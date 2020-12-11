6526ee98b39b5db88ede3d004be6b042968f6784
Navindex <mk@navindex.com.au>
2020-12-12 04:07:22 +0800

    Project settings and copyright notes

--- src/Helpers/object_helper.php
+++ src/Helpers/object_helper.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * Navindex:ModelX.
+ * Model extension for CodeIgniter 4.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Navindex Pty Ltd
+ * @copyright Copyright (c) 2020 Navindex Pty Ltd
+ */
+
+//--------------------------------------------------------------------
+
 if (!\function_exists('extract_properties')) {
 	/**
 	 * Masks an array by the allowed keys.
--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -1,8 +1,23 @@
 <?php
 
+/**
+ * Navindex:ModelX.
+ * Model extension for CodeIgniter 4.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Navindex Pty Ltd
+ * @copyright Copyright (c) 2020 Navindex Pty Ltd
+ */
+
 namespace Navindex\ModelX\Models;
 
-use CodeIgniter\Database\BaseBuilder;
 use CodeIgniter\Database\BaseResult;
 use CodeIgniter\Database\ConnectionInterface;
 use CodeIgniter\Database\Exceptions\DatabaseException;
